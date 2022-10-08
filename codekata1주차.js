ed65a4c57b51c78c998389fff669bd906fa85c36
a <a@aui-MacBookAir.local>
2022-10-08 15:12:04 +0900

    codekata 1주차 2일

--- codekata1주차.js
+++ codekata1주차.js
@@ -10,3 +10,17 @@ const twoSum = (nums, target) => {
     } return result
     // 아래 코드를 작성해주세요.
   }
+
+//1주차 2일 (35분 클리어)
+const reverse = x => {
+  let a = String(x).split('')
+  if (x < 0) { a.shift() }
+  let result = []
+  for (let i in a) {
+    result.push(a[a.length - i - 1])
+  }
+  if (result[0] === "0") { result.shift() }
+  let result2 = Number(result.join(""))
+  if (x < 0) { result2 = -result2 }
+  return result2
+}
