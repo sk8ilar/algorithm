fc76bf2405e461029c9da852f87edd24d961e1f2
alwaysVoid <104767198+sk8ilar@users.noreply.github.com>
2022-10-19 13:26:20 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    for(var answer = 0;answer<500;answer++){
+    if(num%2==0){
+      num = num/2;
+    }
+    else if(num==1){
+        return answer;
+    }
+    else if(num%2==1){
+      num = (num*3)+1;
+    }
+  }
+  //if(answer == 500){return -1;}
+    return -1;
+}
